6765cb7491dee2b29f2ef8fced2e56d6462a026a
Emilio <emilio.puga@live.com>
2025-10-26 04:16:30 -0600

    Add files via upload

---
+++ ANT-icipa/ANT-icipa/ANT_icipaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ANT_icipaApp.swift
+//  ANT-icipa
+//
+//  Created by Emilio Puga on 26/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ANT_icipaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
